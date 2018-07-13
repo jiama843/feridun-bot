98d2c5782e937e9f09090dad38673f0d46c8d9b9
Jia Ma <jiama843@gmail.com>
2018-07-13 00:56:19 -0400

    1st commit
    Basic functions and server setup

---
+++ bot.js
@@ -0,0 +1,66 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(new logger.transports.Console());
+logger.add(new logger.transports.Console(), {
+    colorize: true
+});
+
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+            case 'goose':
+                bot.send('Here is Mr.goose', {
+                    files: [{
+                        attachment: "/Users/John/Documents/Feridun Bot/retardgoose.png",
+                        name: 'retardgoose.png'
+                    }]
+                })
+                .then(console.log)
+                .catch(console.error);
+            break;
+         }
+     }
+});
+
+/*bot.on('message', message => {
+    var prefix = '!'
+    var msg = message.content;
+
+    if (msg === prefix + 'goose') {
+        message.channel.send('Here is Mr.goose', {
+            files: [
+                "retardgoose.png"
+            ]
+        });
+    }
+});*/
